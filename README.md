d8080211f98a0673c9f841535525ebe4c3863a3f
viren1993 <parmarviren16@gmail.com>
2025-12-09 22:03:01 +0530

    updaet the code

---
+++ README.md
@@ -0,0 +1,6 @@
+```bash
+git clone git@github.com:designcombo/react-video-editor.git
+cd react-video-editor
+pnpm install
+pnpm dev
+```
---
+++ src/features/editor/editor.tsx
@@ -0,0 +1,191 @@
+"use client";
+import Timeline from "./timeline";
+import useStore from "./store/use-store";
+import Navbar from "./navbar";
+import useTimelineEvents from "./hooks/use-timeline-events";
+import Scene from "./scene";
+import { SceneRef } from "./scene/scene.types";
+import StateManager, { DESIGN_LOAD } from "@designcombo/state";
+import { useEffect, useRef, useState } from "react";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { ImperativePanelHandle } from "react-resizable-panels";
+import { getCompactFontData, loadFonts } from "./utils/fonts";
+import { SECONDARY_FONT, SECONDARY_FONT_URL } from "./constants/constants";
+import MenuList from "./menu-list";
+import { MenuItem } from "./menu-item";
+import { ControlItem } from "./control-item";
+import CropModal from "./crop-modal/crop-modal";
+import useDataState from "./store/use-data-state";
+import { FONTS } from "./data/fonts";
+import FloatingControl from "./control-item/floating-controls/floating-control";
+import { useSceneStore } from "@/store/use-scene-store";
+import { dispatch } from "@designcombo/events";
+import MenuListHorizontal from "./menu-list-horizontal";
+import { useIsLargeScreen } from "@/hooks/use-media-query";
+import { ITrackItem } from "@designcombo/types";
+import useLayoutStore from "./store/use-layout-store";
+import ControlItemHorizontal from "./control-item-horizontal";
+import { design } from "./mock";
+
+const stateManager = new StateManager({
+  size: {
+    width: 1080,
+    height: 1920,
+  },
+});
+
+const Editor = ({ tempId, id }: { tempId?: string; id?: string }) => {
+  const [projectName, setProjectName] = useState<string>("Untitled video");
+  const timelinePanelRef = useRef<ImperativePanelHandle>(null);
+  const sceneRef = useRef<SceneRef>(null);
+  const { timeline, playerRef } = useStore();
+  const { activeIds, trackItemsMap, transitionsMap } = useStore();
+  const [loaded, setLoaded] = useState(false);
+  const [trackItem, setTrackItem] = useState<ITrackItem | null>(null);
+  const {
+    setTrackItem: setLayoutTrackItem,
+    setFloatingControl,
+    setLabelControlItem,
+    setTypeControlItem,
+  } = useLayoutStore();
+  const isLargeScreen = useIsLargeScreen();
+
+  useTimelineEvents();
+
+  const { setCompactFonts, setFonts } = useDataState();
+
+  // useEffect(() => {
+  //   dispatch(DESIGN_LOAD, { payload: design });
+  // }, []);
+
+  useEffect(() => {
+    setCompactFonts(getCompactFontData(FONTS));
+    setFonts(FONTS);
+  }, []);
+
+  useEffect(() => {
+    loadFonts([
+      {
+        name: SECONDARY_FONT,
+        url: SECONDARY_FONT_URL,
+      },
+    ]);
+  }, []);
+
+  useEffect(() => {
+    const screenHeight = window.innerHeight;
+    const desiredHeight = 300;
+    const percentage = (desiredHeight / screenHeight) * 100;
+    timelinePanelRef.current?.resize(percentage);
+  }, []);
+
+  const handleTimelineResize = () => {
+    const timelineContainer = document.getElementById("timeline-container");
+    if (!timelineContainer) return;
+
+    timeline?.resize(
+      {
+        height: timelineContainer.clientHeight - 90,
+        width: timelineContainer.clientWidth - 40,
+      },
+      {
+        force: true,
+      }
+    );
+
+    // Trigger zoom recalculation when timeline is resized
+    setTimeout(() => {
+      sceneRef.current?.recalculateZoom();
+    }, 100);
+  };
+
+  useEffect(() => {
+    const onResize = () => handleTimelineResize();
+    window.addEventListener("resize", onResize);
+    return () => window.removeEventListener("resize", onResize);
+  }, [timeline]);
+
+  useEffect(() => {
+    if (activeIds.length === 1) {
+      const [id] = activeIds;
+      const trackItem = trackItemsMap[id];
+      if (trackItem) {
+        setTrackItem(trackItem);
+        setLayoutTrackItem(trackItem);
+      } else console.log(transitionsMap[id]);
+    } else {
+      setTrackItem(null);
+      setLayoutTrackItem(null);
+    }
+  }, [activeIds, trackItemsMap]);
+
+  useEffect(() => {
+    setFloatingControl("");
+    setLabelControlItem("");
+    setTypeControlItem("");
+  }, [isLargeScreen]);
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
+  return (
+    <div className="flex h-screen w-screen flex-col">
+      {/* navbar header show  */}
+      <Navbar
+        projectName={projectName}
+        user={null}
+        stateManager={stateManager}
+        setProjectName={setProjectName}
+      />
+      <div className="flex flex-1">
+        {isLargeScreen && (
+          <div className="bg-muted  flex flex-none border-r border-border/80 h-[calc(100vh-44px)]">
+            <MenuList />
+            <MenuItem />
+          </div>
+        )}
+        <ResizablePanelGroup style={{ flex: 1 }} direction="vertical">
+          <ResizablePanel className="relative" defaultSize={70}>
+            <FloatingControl />
+            <div className="flex h-full flex-1">
+              {/* Sidebar only on large screens - conditionally mounted */}
+              <div
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  position: "relative",
+                  flex: 1,
+                  overflow: "hidden",
+                }}
+              >
+                <CropModal />
+                {/* main screen show  */}
+                <Scene ref={sceneRef} stateManager={stateManager} />
+              </div>
+            </div>
+          </ResizablePanel>
+          <ResizableHandle />
+          <ResizablePanel
+            className="min-h-[50px]"
+            ref={timelinePanelRef}
+            defaultSize={30}
+            onResize={handleTimelineResize}
+          >
+            {playerRef && <Timeline stateManager={stateManager} />}
+          </ResizablePanel>
+          {!isLargeScreen && !trackItem && loaded && <MenuListHorizontal />}
+          {!isLargeScreen && trackItem && <ControlItemHorizontal />}
+        </ResizablePanelGroup>
+        {/* text des controls edit updated the value */}
+        <ControlItem />
+      </div>
+    </div>
+  );
+};
+
+export default Editor;
